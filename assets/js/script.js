3d62c4053fd7f2dfbcda7c77078f9d38f21d8a5a
samikhawja <skhawja11@gmail.com>
2021-09-30 12:17:05 -0700

    fixed ongoing time for webpage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,13 @@
 var today = moment();
 
-$(".date").text(today.format("MMM Do, YYYY"));
-$(".time").text(today.format("LTS"));
+$("#date").text(today.format("MMM Do, YYYY"));
+// $(".time").text(today.format("LTS"));
+setInterval(myTimer);
+
+function myTimer() {
+  const d = new Date();
+  document.getElementById("time").innerHTML = d.toLocaleTimeString();
+}
 // window.setInterval(function () {
 //     $(".time").html(today.format("LTS"));
 // }, 1000);
