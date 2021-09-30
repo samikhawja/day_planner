8fecb4d42f09750de7e585557069b4b2d0ce3419
samikhawja <skhawja11@gmail.com>
2021-09-29 20:55:41 -0700

    added date and time but still need to make it ongoing/live

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,7 @@
-console.log("Sami")
+var today = moment();
+
+$(".date").text(today.format("MMM Do, YYYY"));
+$(".time").text(today.format("LTS"));
+// window.setInterval(function () {
+//     $(".time").html(today.format("LTS"));
+// }, 1000);
